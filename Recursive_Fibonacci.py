e6571dd7c14c1e3d3ac44afd187e8796b5da9abd
KadirErbas <93327468+KadirErbas@users.noreply.github.com>
2023-04-13 18:48:15 +0300

    Add files via upload

---
+++ Recursive_Fibonacci.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    listem.append(n)
+    return fibonacci(n-1) + fibonacci(n-2)      
+
+
+# In[11]:
+
+
+print(fibonacci(0), fibonacci(1), fibonacci(2))
+
+
+# In[ ]:
+
+
+
+
